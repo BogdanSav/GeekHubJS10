0c77a1c68bafb47ef7744a29d8965b0ba580b4a6
BogdanSav <bohdan16@live.com>
2020-11-09 23:47:47 +0200

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+let array = [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2, -3, -1];
+
+function sum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    console.log(sum);
+}
+
+function min(array) {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+
+    }
+    console.log(min);
+}
+
+function max(array) {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+
+    }
+    console.log(max);
+}
+sum(array);
+min(array);
+max(array);
