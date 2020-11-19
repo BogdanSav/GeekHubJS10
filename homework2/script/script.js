ed8a90bca8396bcee04d15e5c3347fee3793e993
BogdanSav <bohdan16@live.com>
2020-11-19 22:39:09 +0200

    tamagochi in process

--- homework2/script/script.js
+++ homework2/script/script.js
@@ -11,17 +11,29 @@ function cycleFor(start, condidtion, step) {
 cycleFor(2, 10, 1);
 //#endregion
 //#region sum(1)(2)(3)
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    }
+}
+
+function sumFunc(a, b, c) {
+    return a + b + c;
+}
+let sum = curry(sumFunc);
+console.log(sum(1)(2)(3));
+//#endregion
+//#region calculate
 
 
 //#endregion
-//#region calculate
-function calculate(a) {
-    this.firstArg = a;
-    this.add = function(b) {
-        return this.firstArg + b;
+//region array methods
 
-    }
-}
-let calc = new calculate(3);
-console.log(calc.add(2));
+
 //#endregion
