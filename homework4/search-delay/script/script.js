51431304adc1762c453fbb090f188536a004a7fe
BogdanSav <bohdan16@live.com>
2020-12-26 22:52:58 +0200

    homework4.5

---
+++ homework4/search-delay/script/script.js
@@ -0,0 +1,21 @@
+let input = document.getElementById("input");
+let timeout;
+input.addEventListener("keyup", (e) => {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => { console.log("done") }, 3000);
+
+});
+
+
+//
+// let promice = function(res, rej) {
+//     res.call(null);
+//     rej.call(null);
+// }
+// promice()
+//     .then(
+//         result => console.log(result),
+//         error => { new Error(console.error(error)) }
+//     )
