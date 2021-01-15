0c2f366ae8bf8267f44136c55a3e636807ae55e6
BogdanSav <bohdan16@live.com>
2021-01-15 23:07:09 +0200

    todo classes done

--- homework5/todolist_class/src/App.js
+++ homework5/todolist_class/src/App.js
@@ -1,41 +1,55 @@
 import React, { Component } from 'react';
 import ToDoItem from './toDoItem';
-
+import { Container, Grid, Input,  Button } from '@material-ui/core';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.InputRef = React.createRef();
     this.state = {
       toDoItem: [],
-      text: "",
     }
-    this.items= undefined;
+    this.items=undefined;
+    
   }
-  newItem = () => {
-    let toDoItem = [...this.state.toDoItem];
-    toDoItem.push(<ToDoItem text = {this.state.text}/> );
-    this.setState({ toDoItem });
+  newItem=()=> {
+    console.log(this.InputRef.current.children[0].value);
+    let toDoItem =[...this.state.toDoItem];
+    toDoItem.push("");
+    this.setState({toDoItem}); 
+    setTimeout(()=>{ this.InputRef.current.children[0].value = ""},10);
+    clearTimeout();
+   
   }
-  deleteItem=()=>{ 
-    let toDoItem = [...this.state.toDoItem];
-    toDoItem.pop();
-    this.setState({ toDoItem });
-  
-  }
-  inputOnChange= (e)=>{
-    this.setState({text:e.target.value})
+  deleteItem=(e)=>{
+    let toDoItem =[...this.state.toDoItem];
+    toDoItem.splice(e.target.getAttribute('index'),1);
+    this.setState({toDoItem}); 
+    console.log();
   }
   render() {
     return (
-      <div className="container p-4">
-        <input type="text" onChange={this.inputOnChange} />
-        <button className="btn btn-primary" onClick={this.newItem}>add New</button>
-        {this.state.toDoItem}
-      </div>
+      <Container align="center" maxWidth="sm" style={{border: "1px solid black", borderRadius: "10px 10px", padding:"15px", marginTop:"10px" }}>
+        <h1>ToDo</h1>
+        <Grid container justify="center" spacing={3}>
+          <Grid item>
+            <Input type="text" ref={this.InputRef}/>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="secondary" onClick={this.newItem}>Add new</Button>
+          </Grid>
+        </Grid>
+        {this.items = this.state.toDoItem.map((item, index)=>(
+           <ToDoItem text={this.InputRef.current.children[0].value} delete={this.deleteItem} index={index} />
+           
+        ))}
+      </Container>
+
 
     );
   }
+
 }
 
 export default App;
--- homework5/todolist_class/src/toDoItem.js
+++ homework5/todolist_class/src/toDoItem.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Grid,  Checkbox, TextField, } from '@material-ui/core';
 
-class ToDoItem extends React.Component{
-    constructor(props){
+export default class ToDoItem extends React.Component {
+    constructor(props) {
         super(props)
+        this.textRef = React.createRef();
+        this.checkRef = React.createRef();
+    }
+
+    isChecked = () => {
+        let text = this.textRef.current.children[0].children[0];
+        if (this.checkRef.current.children[0].children[0].checked) {
+            text.style.textDecoration = "line-through";
+        } else text.style.textDecoration = "none";
 
     }
-    render(){
-        return(
-            <div className="input-group mb-3">
-                <span className="input-group-text">
-                <input className="form-check-input" type="checkbox" />  
-                {this.props.text}</span>
-                
-            </div>
+    render() {
+
+        return (
+            <Grid container spacing={1} justify="space-around" style={{ marginTop: "10px", border: "1px solid black", borderRadius: "10px 10px", padding: "2px" }}>
+                <Grid item >  <Checkbox style={{ color: "green" }} onClick={this.isChecked} ref={this.checkRef} /></Grid>
+                <Grid item > <TextField id="filled-basic" value={this.props.text} ref={this.textRef} /></Grid>
+                <Grid item > <Button variant="contained" color="primary" onClick={this.props.delete} index={this.props.index}> delete</Button></Grid>
+
+            </Grid>
+
         );
-    };
+    }
+
 }
-export default ToDoItem;
+
+
+
