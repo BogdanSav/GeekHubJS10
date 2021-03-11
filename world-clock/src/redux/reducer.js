bc628383280e7e622d015f58147a4c97a3a1faf4
BogdanSav <bohdan16@live.com>
2021-03-11 19:39:50 +0200

    world-clock bug fixes 1.1

--- world-clock/src/redux/reducer.js
+++ world-clock/src/redux/reducer.js
@@ -3,7 +3,7 @@ import { CHANGE_TIMEZONE } from "./actions";
 export default function reducer(state = "Europe/Kiev", action) {
     switch (action.type) {
         case CHANGE_TIMEZONE:
-            state = action.payload;
+            return state = action.payload;
         default:
             return state;
     }
