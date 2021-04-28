da3664b315e082791113437ab0297ec66938ac29
BogdanSav <bohdan16@live.com>
2021-04-28 10:05:56 +0300

    fixing errors 1.2

--- get-apis/src/components/CardContainer.js
+++ get-apis/src/components/CardContainer.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-
-import { Grid } from "@material-ui/core"
-import Card from "./Card"
+import {v4} from 'uuid';
+import { Grid } from "@material-ui/core";
+import Card from "./Card";
 
 function CardContainer() {
-   
-   
     const data = useSelector(state=>state.apis);
 
     return (
         <Grid item>
             <Grid container direction="row" justify="space-around" alignItems="flex-start" spacing={2} >
                 {
-                    data.map((data, index) => (
-                        <Card key={index} content={data} />
+                    data.map((data) => (
+                        <Card key={v4} content={data} />
                     ))
                 }
             </Grid>
